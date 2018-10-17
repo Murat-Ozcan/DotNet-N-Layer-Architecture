6e0b39861cbf36d3d604af1431abd4eafad32594
Murat-Ozcan <38462764+Murat-Ozcan@users.noreply.github.com>
2018-10-17 16:33:30 +0300

    "LogModel" development

---
+++ source/4-Model/MRTFramework.Model.InApps/LogModel/LogDetail.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MRTFramework.Model.InApps.LogModel
+{
+    public class LogDetail
+    {
+        public string NameSpace { get; set; }
+        public string MethodName { get; set; }
+        public string CodeLine { get; set; }
+        public string CustomMessage { get; set; }
+        public string AspectName { get; set; }
+        public List<LogParameter> Parameters { get; set; }
+    }
+}
---
+++ source/4-Model/MRTFramework.Model.InApps/LogModel/LogParameter.cs
@@ -0,0 +1,9 @@
+﻿namespace MRTFramework.Model.InApps.LogModel
+{
+    public class LogParameter
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public object Value { get; set; }
+    }
+}
