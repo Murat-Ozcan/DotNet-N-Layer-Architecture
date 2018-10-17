fae0a76d028794b9b41c25c46ac24b7040e85799
Murat-Ozcan <38462764+Murat-Ozcan@users.noreply.github.com>
2018-10-17 17:25:46 +0300

    "IUserDao" interface development

---
+++ source/3-DataAccessLayer/Abstract/MRTFramework.DataAccessLayer.DAOInterfaces/Repositories/IUserDao.cs
@@ -0,0 +1,12 @@
+﻿using MRTFramework.CrossCuttingConcern.Utils.Interfaces;
+using MRTFramework.Model.BaseModels.Concrete;
+using MRTFramework.Model.Requests;
+using MRTFramework.Model.Responses;
+
+namespace MRTFramework.DataAccessLayer.DAOInterfaces.Repositories
+{
+    public interface IUserDao : ISyncRepository<User>, IAsyncRepository<User>
+    {
+        AuthenticationUserDto AuthenticationUser(LoginUserDto authenticationUser);
+    }
+}
