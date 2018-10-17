08d39d935e9e34a51b0f89cccfaee162a5f59b2d
Murat-Ozcan <38462764+Murat-Ozcan@users.noreply.github.com>
2018-10-17 16:30:42 +0300

    "ISyncRepository" and "IAsyncRepository" development

---
+++ source/5-CrossCuttingConcern/MRTFramework.CrossCuttingConcern.Utils/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using MRTFramework.Model.BaseModels.Abstract;
+
+namespace MRTFramework.CrossCuttingConcern.Utils.Interfaces
+{
+    public interface IAsyncRepository<T> where T : class, IBaseEntity, new()
+    {
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entity);
+        Task UpdateAsync(T entity);
+        Task UpdateRangeAsync(IEnumerable<T> entity);
+        Task DeleteAsync(T entity);
+        Task CommitAsync();
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate = null);
+        Task<int> CountAsync(Expression<Func<T, bool>> predicate = null);
+        Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate = null);
+        Task<T> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate = null);
+        Task<T> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> ToAllListAsync(Expression<Func<T, bool>> predicate = null);
+        Task<IEnumerable<T>> GetListAsync(Expression<Func<T, bool>> predicate, List<string> includes);
+        Task<IEnumerable<T>> GetListOrderByAsync(Expression<Func<T, bool>> predicate, Func<T, object> sortColumn, List<string> includes);
+        Task<IEnumerable<T>> GetListOrderByDescAsync(Expression<Func<T, bool>> predicate, Func<T, object> sortColumn, List<string> includes);
+        Task<IEnumerable<T>> GetListByPageAsync(Expression<Func<T, bool>> predicate, ushort pageIndex, ushort pageSize, List<string> includes);
+        Task<IEnumerable<T>> GetListByPageOrderByAsync(Expression<Func<T, bool>> predicate, Func<T, object> sortColumn, ushort pageIndex, ushort pageSize, List<string> includes);
+        Task<IEnumerable<T>> GetListByPageOrderByDescAsync(Expression<Func<T, bool>> predicate, Func<T, object> sortColumn, ushort pageIndex, ushort pageSize, List<string> includes);
+    }
+}
---
+++ source/5-CrossCuttingConcern/MRTFramework.CrossCuttingConcern.Utils/Interfaces/ISyncRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using MRTFramework.Model.BaseModels.Abstract;
+
+namespace MRTFramework.CrossCuttingConcern.Utils.Interfaces
+{
+    public interface ISyncRepository<T> where T : class, IBaseEntity, new()
+    {
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entity);
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entity);
+        void Delete(T entity);
+        void Commit();
+        bool Any(Expression<Func<T, bool>> predicate = null);
+        int Count(Expression<Func<T, bool>> predicate = null);
+        T FirstOrDefault(Expression<Func<T, bool>> predicate = null);
+        T SingleOrDefault(Expression<Func<T, bool>> predicate = null);
+        T Find(Expression<Func<T, bool>> predicate);
+        IEnumerable<T> ToAllList(Expression<Func<T, bool>> predicate = null);
+        IEnumerable<T> GetList(Expression<Func<T, bool>> predicate, List<string> includes);
+        IEnumerable<T> GetListOrderBy(Expression<Func<T, bool>> predicate, Func<T, object> sortColumn, List<string> includes);
+        IEnumerable<T> GetListOrderByDesc(Expression<Func<T, bool>> predicate, Func<T, object> sortColumn, List<string> includes);
+        IEnumerable<T> GetListByPage(Expression<Func<T, bool>> predicate, ushort pageIndex, ushort pageSize, List<string> includes);
+        IEnumerable<T> GetListByPageOrderBy(Expression<Func<T, bool>> predicate, Func<T, object> sortColumn, ushort pageIndex, ushort pageSize, List<string> includes);
+        IEnumerable<T> GetListByPageOrderByDesc(Expression<Func<T, bool>> predicate, Func<T, object> sortColumn, ushort pageIndex, ushort pageSize, List<string> includes);
+        IQueryable<T> GetListQueryable(Expression<Func<T, bool>> predicate, List<string> includes);
+    }
+}
