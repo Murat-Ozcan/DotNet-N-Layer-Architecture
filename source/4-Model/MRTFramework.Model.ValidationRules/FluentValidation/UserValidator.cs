d1c87e27d76087b3394d4a465c28f2e2306541ee
Murat-Ozcan <38462764+Murat-Ozcan@users.noreply.github.com>
2018-10-17 16:52:17 +0300

    "UserValidator" class for "ValidationRules" class library development

---
+++ source/4-Model/MRTFramework.Model.ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using MRTFramework.Model.BaseModels.Concrete;
+
+namespace MRTFramework.Model.ValidationRules.FluentValidation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.Firstname).NotEmpty().NotNull();
+            RuleFor(x => x.Surname).NotEmpty().NotNull();
+            RuleFor(x => x.Age).NotEmpty().NotNull().GreaterThan(0);
+        }
+    }
+}
