0439aa14b1651cafdba9e6137f1ad9927f807f31
Murat-Ozcan <38462764+Murat-Ozcan@users.noreply.github.com>
2018-10-18 10:31:34 +0300

    "DependencyInjector class" for "DependencyInjector" class library development

---
+++ source/5-CrossCuttingConcern/MRTFramework.CrossCuttingConcern.DependencyInjection/DependencyInjector.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MRTFramework.CrossCuttingConcern.Security;
+using MRTFramework.DataAccessLayer.DAOInterfaces.Repositories;
+using MRTFramework.DataAccessLayer.DAOInterfaces.UnitOfWork;
+using MRTFramework.DataAccessLayer.EntityFrameworkCore.Repositories;
+using MRTFramework.DataAccessLayer.EntityFrameworkCore.UnitOfWork;
+
+namespace MRTFramework.CrossCuttingConcern.DependencyInjection
+{
+    public class DependencyInjector
+    {
+        private static IServiceProvider ServiceProvider { get; set; }
+
+        private static IServiceCollection Services { get; set; }
+
+        public static void AddDbContext<T>(string connectionString) where T : DbContext
+        {
+            Services.AddDbContextPool<T>(
+                x => x.UseSqlServer
+                    (connectionString, q => q.MigrationsAssembly("MRTFramework.SharedUI.Api"))
+            );
+
+            var context = GetService<T>();
+            context.Database.EnsureCreated();
+            context.Database.Migrate();
+        }
+
+        public static T GetService<T>()
+        {
+            Services = Services ?? RegisterServices();
+            ServiceProvider = ServiceProvider ?? Services.BuildServiceProvider();
+            return ServiceProvider.GetService<T>();
+        }
+
+        public static IServiceCollection RegisterServices()
+        {
+            return RegisterServices(new ServiceCollection());
+        }
+
+        public static IServiceCollection RegisterServices(IServiceCollection service)
+        {
+            Services = service;
+
+            // Solution - Data Access - Concrete / MRTFramework.DataAccess.EntityFrameworkCore
+
+            Services.AddScoped<IDatabaseUnitOfWork, EfcDatabaseUnitOfWork>();
+            Services.AddScoped<IUserDao, EfcUserDao>();
+
+            // Solution - Business - Concrete / MRTFramework.Business.Domain
+
+            //Services.AddScoped<IUserService, UserManager>();
+            //Services.AddScoped<IAuthentication, AuthenticationManager>();
+
+            // Solution.CrossCutting
+            Services.AddScoped<IJsonWebToken, JsonWebToken>();
+            return Services;
+        }
+    }
+}
