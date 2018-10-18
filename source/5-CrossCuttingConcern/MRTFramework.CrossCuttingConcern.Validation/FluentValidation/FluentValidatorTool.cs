7c54c1607d56a882298ebbcb4c96cd9dac219bb0
Murat-Ozcan <38462764+Murat-Ozcan@users.noreply.github.com>
2018-10-18 09:45:37 +0300

    "FluentValidatorTool" for Validation class librray development

---
+++ source/5-CrossCuttingConcern/MRTFramework.CrossCuttingConcern.Validation/FluentValidation/FluentValidatorTool.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace MRTFramework.CrossCuttingConcern.Validation.FluentValidation
+{
+    public class FluentValidatorTool
+    {
+        public static void FluentValidate(IValidator validator, object entity)
+        {
+            var result = validator.Validate(entity);
+            if (result.Errors.Count > 0)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
