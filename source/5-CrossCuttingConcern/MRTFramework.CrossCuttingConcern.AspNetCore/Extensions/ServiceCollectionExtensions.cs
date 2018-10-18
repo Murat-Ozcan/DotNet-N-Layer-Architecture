43a81cfefaff639cf646f9ac2c3b2053fe063b67
Murat-Ozcan <38462764+Murat-Ozcan@users.noreply.github.com>
2018-10-18 10:51:59 +0300

    "ServiceCollectionExtensions" for "AspNetCore" class library development

---
+++ source/5-CrossCuttingConcern/MRTFramework.CrossCuttingConcern.AspNetCore/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using MRTFramework.CrossCuttingConcern.DependencyInjection;
+using MRTFramework.CrossCuttingConcern.Security;
+
+namespace MRTFramework.CrossCuttingConcern.AspNetCore.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddAuthenticationCustom(this IServiceCollection services)
+        {
+            var jsonWebToken = DependencyInjector.GetService<IJsonWebToken>();
+
+            void JwtBearer(JwtBearerOptions jwtBearer)
+            {
+                jwtBearer.TokenValidationParameters = jsonWebToken.TokenValidationParameters;
+            }
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(JwtBearer);
+        }
+    }
+}
