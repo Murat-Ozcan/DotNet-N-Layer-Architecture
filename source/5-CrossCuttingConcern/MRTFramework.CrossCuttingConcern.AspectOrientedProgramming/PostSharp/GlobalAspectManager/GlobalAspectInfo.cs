140f01af3b55c6a01ec76ebd4d718108aea8060d
Murat-Ozcan <38462764+Murat-Ozcan@users.noreply.github.com>
2018-10-18 11:21:12 +0300

    "GlobalAspectInfo" for "AspectOrientedProgramming" class library development

---
+++ source/5-CrossCuttingConcern/MRTFramework.CrossCuttingConcern.AspectOrientedProgramming/PostSharp/GlobalAspectManager/GlobalAspectInfo.cs
@@ -0,0 +1,10 @@
+﻿using MRTFramework.CrossCuttingConcern.AspectOrientedProgramming.PostSharp.ExceptionAspect;
+using MRTFramework.CrossCuttingConcern.AspectOrientedProgramming.PostSharp.PerformanceAspect;
+using MRTFramework.CrossCuttingConcern.Logging.Concrete.NLog;
+
+//[assembly: ExceptionAspect(loggerServiceType: typeof(NLogService), loggerProcessType: typeof(NLogDatabaseLogger), AttributeTargetTypes = "MRTFramework.Business.Domain.Managers.*")]
+//[assembly: LogAspect(loggerServiceType: typeof(NLogService), loggerProcessType: typeof(NLogDatabaseLogger), AttributeTargetTypes = "MRTFramework.Business.Domain.Managers.*")]
+//[assembly: PerformanceAspect(loggerServiceType: typeof(NLogService), loggerProcessType: typeof(NLogDatabaseLogger), interval: 0, AttributeTargetTypes = "MRTFramework.Business.Domain.Managers.*")]
+[assembly: PerformanceAspect(loggerServiceType: typeof(NLogService), loggerProcessType: typeof(NLogDatabaseLogger), AttributeTargetTypes = "MRTFramework.Business.Domain.Managers.*")]
+
+
