b8009605cee063418cbba6cfe90d8e4c1064abe5
Murat-Ozcan <38462764+Murat-Ozcan@users.noreply.github.com>
2018-10-22 14:26:47 +0300

    UnitOfWork development

--- source/3-DataAccessLayer/Concrete/MRTFramework.DataAccessLayer.EntityFrameworkCore/UnitOfWork/EfcDatabaseUnitOfWork.cs
+++ source/3-DataAccessLayer/Concrete/MRTFramework.DataAccessLayer.EntityFrameworkCore/UnitOfWork/EfcDatabaseUnitOfWork.cs
@@ -1,18 +1,20 @@
 ﻿using MRTFramework.DataAccessLayer.DAOInterfaces.Repositories;
 using MRTFramework.DataAccessLayer.DAOInterfaces.UnitOfWork;
 using MRTFramework.DataAccessLayer.EntityFrameworkCore.Context;
+using MRTFramework.DataAccessLayer.EntityFrameworkCore.Repositories;
 
 namespace MRTFramework.DataAccessLayer.EntityFrameworkCore.UnitOfWork
 {
     public class EfcDatabaseUnitOfWork : IDatabaseUnitOfWork
     {
-        public EfcDatabaseUnitOfWork(IUserDao userDal, NorthwindContext context)
+        private IUserDao _userDal;
+
+        public EfcDatabaseUnitOfWork(NorthwindContext context)
         {
-            UserDal = userDal;
             Context = context;
         }
 
-        public IUserDao UserDal { get; }
+        public IUserDao UserDal => _userDal ?? (_userDal = new EfcUserDao(Context));
         private NorthwindContext Context { get; }
 
         public void SaveChanges()
